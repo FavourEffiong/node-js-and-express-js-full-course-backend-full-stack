96864d821e2fd707a7202adb7d98e27be917170a
Favour2008 <145934527+FavourEffiong@users.noreply.github.com>
2025-06-10 16:36:21 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const amount = 9
+// GLOBALS - NO WINDOW !!!!
 
-if (amount < 10) {
-console.log( 'small number' )
-} else {
-console.log('large number')
-}
-
-console.log(`hey it's my first node app!!!`)
+// __dirname - path to current FileSystemDirectoryEntry
+// __filename - file name
+// require - fuction to use modules (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
